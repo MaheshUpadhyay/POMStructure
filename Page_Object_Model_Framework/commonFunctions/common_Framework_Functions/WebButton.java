bbbc42594f5f73fbdd2f43b208e1e446dd03a4b9
Mahesh Upadhyay <mahesh.upadhyay@jci.com>
2018-09-16 13:01:21 +0530

    USABC_Login - Added Comment to WebButton

--- Page_Object_Model_Framework/commonFunctions/common_Framework_Functions/WebButton.java
+++ Page_Object_Model_Framework/commonFunctions/common_Framework_Functions/WebButton.java
@@ -3,7 +3,7 @@ package common_Framework_Functions;
 import org.openqa.selenium.WebElement;
 
 public class WebButton {
-	
+	//This is constructor
 	/*
 	 * This method is to click on Web Button
 	 * @Param : WebElement
