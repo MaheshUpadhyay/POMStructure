60310af1cd66b9567679b94227aed605e26da648
Mahesh <maheshupadhyayqa@gmail.com>
2022-11-22 08:46:42 +0530

    US7654_Login_Feature - Kautubh wanted to automate Login feature

--- Page_Object_Model_Framework/commonFunctions/common_Framework_Functions/WebCheckBox.java
+++ Page_Object_Model_Framework/commonFunctions/common_Framework_Functions/WebCheckBox.java
@@ -1,6 +1,7 @@
 package common_Framework_Functions;
 /*
  *I am updating my login feature for my self
+ * I am updating login feature for Kaustubh
  */
 public class WebCheckBox {
 	public static void click(){
